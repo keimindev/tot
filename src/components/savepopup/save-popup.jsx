8c2d9b53dffd53b6a4483c7b58694910f5a4aee4
keimindev <keimindev@gmail.com>
2024-03-25 16:04:40 +0900

    Add guest mode on Save button

--- src/components/savepopup/save-popup.jsx
+++ src/components/savepopup/save-popup.jsx
@@ -52,22 +52,28 @@ const SavePopup = ({setOpen, time, setTime, setStartTime}) => {
       router.push('/')
     }
 
+    const backToHome = async () => {
+      setOpen(false);
+      setTime(0);
+      setStartTime(null)
+      router.push('/')
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.sections}>
-              <div className={styles.todaySections}>✨오늘의 기록✨</div>
-              {sectionofrecord === 'Reading' && <h3>독서</h3>}
-              {sectionofrecord === 'Study' && <h3>공부</h3>}
-              {sectionofrecord === 'Workout' && <h3>운동</h3>}
-              {sectionofrecord === 'Cook' && <h3>요리</h3>}
+              <div className={styles.todaySections}>✨Today&apos;s Record✨</div>
+              {sectionofrecord === 'Reading' && <h3>Reading</h3>}
+              {sectionofrecord === 'Study' && <h3>Study</h3>}
+              {sectionofrecord === 'Workout' && <h3>Workout</h3>}
+              {sectionofrecord === 'Cook' && <h3>Cook</h3>}
             <div className={styles.time}>{formatTimeClock(time)}</div>
             </div>
             {userInfo.username === 'guest' ? 
             <div>
-              <div>If you want to save your record, Join us!</div>
-              <div>
-              <div>Back to Home</div>
-              <div>Login</div>
+              <div className={styles.jointext}>If you want to save your record, Join us!</div>
+              <div className={styles.btnBox}>
+              <div className={styles.btn} onClick={backToHome}>Back to Home</div>
               </div>
               </div> : 
             <div className={styles.btnBox}>
